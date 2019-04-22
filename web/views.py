a9d9b6a39b366da483be6d7c46508eb06a3652f2
Lirio Kuhnen <lirio_kuhnen@hotmail.com>
2019-04-22 01:46:15 -0300

    base html structure

--- web/views.py
+++ web/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return render(request, 'index.html', {})
+
+
+def signUp(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'sign_up.html', {'form': form})
